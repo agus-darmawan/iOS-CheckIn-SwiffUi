7f9119874d58c72e8993a6a4aabe8f3ac3fb0d7e
agus-darmawan <wayanagus.dr@gmail.com>
2025-06-20 08:08:24 +0700

    Initial Commit

---
+++ CheckIn/CheckInApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckInApp.swift
+//  CheckIn
+//
+//  Created by Darmawan on 20/06/25.
+//
+
+import SwiftUI
+
+@main
+struct CheckInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
